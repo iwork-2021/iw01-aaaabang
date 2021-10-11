8749b54bd2a2a24b299e18d669b80d7b42ebb74c
aaaabang <aaaabang@aaaabangdeMacBook-Air.local>
2021-10-11 10:57:26 +0800

    layout debug

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -9,11 +9,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var displayLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.displayLabel.text! = "00"
     }
 
-
+    
+    
+    var digitOnDisplay : String{
+        get{
+            return self.displayLabel.text!
+        }
+        
+        set{
+            self.displayLabel.text! = newValue
+        }
+    }
+    
+    var inTypingMode = false;
+    
+    @IBAction func numberTouch(_ sender: UIButton) {
+        if inTypingMode {
+            digitOnDisplay = digitOnDisplay + sender.currentTitle!
+        }
+        else {//重新输入新的值
+            digitOnDisplay = sender.currentTitle!
+            inTypingMode = true;
+        }
+    }
+    
+    
+    @IBAction func operatorTouch(_ sender: UIButton) {
+        inTypingMode = false;
+        
+    }
 }
 
